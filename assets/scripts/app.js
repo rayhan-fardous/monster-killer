762d767fa0837913ccda69e878f37f3bb9f3bfca
Md Rayhan ul Fardous <49313477+rayhan-fardous@users.noreply.github.com>
2025-05-25 12:33:36 +0600

    added strongAttack mode

---
+++ assets/scripts/app.js
@@ -0,0 +1,41 @@
+const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
+const MONSTER_ATTACK_VALUE = 14;
+
+let chosenMaxLife = 100;
+let currentMosterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
+
+adjustHealthBars(chosenMaxLife);
+
+function attackMonster(mode) {
+  let maxDamage;
+  if (mode === 'ATTACK') {
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === 'STRONG_ATTACK') {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+  const damage = dealMonsterDamage(maxDamage);
+  currentMosterHealth -= damage;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+  if (currentMosterHealth <= 0 && currentPlayerHealth > 0) {
+    alert('You Won!');
+  } else if (currentPlayerHealth <= 0 && currentMosterHealth > 0) {
+    alert('You Lost!');
+  } else if (currentPlayerHealth <= 0 && currentMosterHealth <= 0) {
+    alert('You have a draw!');
+  }
+}
+
+function attackHandler() {
+  attackMonster('ATTACK');
+}
+
+function strongAttackHandler() {
+  attackMonster('STRONG_ATTACK');
+}
+
+
+attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
